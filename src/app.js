2ae988d5eafbbcff180f5fd894d0028abcedc112
FelipeAlvarado01 <afvalvarado@gmail.com>
2025-03-05 00:03:43 -0500

    formulario para crear un nuvo proyecto

--- src/app.js
+++ src/app.js
@@ -39,3 +39,41 @@ document.addEventListener("click", (event) => {
     userMenu.classList.remove("active");
   }
 });
+
+/*----------create new project------------*/
+const createProject = document.querySelector(".add-button");
+const newProject = document.querySelector(".add-project");
+
+createProject.addEventListener("click", () => {
+  newProject.classList.toggle("active");
+});
+
+const closePanelCreateProject = document.querySelector(".close-button");
+
+closePanelCreateProject.addEventListener("click", () => {
+  newProject.classList.remove("active");
+});
+
+document.addEventListener("click", (event) => {
+  if (
+    !createProject.contains(event.target) &&
+    !newProject.contains(event.target)
+  ) {
+    newProject.classList.remove("active");
+  }
+});
+
+/*----------save new task at the localstorage------------*/
+const createTask = document.querySelector(".form-add-project");
+
+createTask.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const project = {
+    name: document.querySelector(".project-name").value,
+    description: document.querySelector(".project-description").value,
+    dateStart: document.querySelector(".start-date").value,
+    dateEnd: document.querySelector(".end-date").value,
+  };
+
+  console.log(project);
+});
